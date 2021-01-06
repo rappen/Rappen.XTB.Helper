250c03db47c7d9e8e1a674cc6a79c826444212df
Jonas Rapp <jonas@jonasr.app>
2021-01-06 23:39:01 +0100

    Stub for XRMEntityComboBox

---
+++ Controls/XRMEntityComboBox.cs
@@ -0,0 +1,161 @@
+﻿using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Messages;
+using Microsoft.Xrm.Sdk.Metadata;
+using Microsoft.Xrm.Sdk.Query;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Windows.Forms;
+using xrmtb.XrmToolBox.Controls.Helper;
+
+namespace Rappen.XTB.Helpers.Controls
+{
+    public delegate void ProgressUpdate(string message);
+    public delegate void RetrieveComplete(int itemCount, Entity FirstItem);
+
+    public partial class XRMEntityComboBox : ComboBox
+    {
+        #region Private properties
+        private string displayFormat = string.Empty;
+        private IEnumerable<EntityMetadata> entities;
+        private IOrganizationService organizationService;
+        #endregion
+
+        #region Public Constructors
+
+        public XRMEntityComboBox()
+        {
+            InitializeComponent();
+        }
+
+        #endregion Public Constructors
+
+        #region Public Properties
+
+        [Category("Data")]
+        [Description("Indicates the source of data (EntityCollection) for the CDSDataComboBox control.")]
+        public new object DataSource
+        {
+            get
+            {
+                if (entities != null)
+                {
+                    return entities;
+                }
+                return base.DataSource;
+            }
+            set
+            {
+                if (value is IEnumerable<EntityMetadata> newentities)
+                {
+                    entities = newentities;
+                    Refresh();
+                }
+            }
+        }
+
+        [Category("Data")]
+        [DisplayName("Display Format")]
+        [Description("Single attribute from datasource to display for items, or use {{attributename}} syntax freely.")]
+        public string DisplayFormat
+        {
+            get { return displayFormat; }
+            set
+            {
+                if (value != displayFormat)
+                {
+                    displayFormat = value;
+                    Refresh();
+                }
+            }
+        }
+
+        [Browsable(false)]
+        public IOrganizationService OrganizationService
+        {
+            get { return organizationService; }
+            set
+            {
+                organizationService = value;
+                Refresh();
+            }
+        }
+
+        [Browsable(false)]
+        public Entity SelectedEntity => (SelectedItem is EntityWrapper item) ? item.Entity : null;
+
+        #endregion Public Properties
+
+        #region Public Methods
+
+        public override void Refresh()
+        {
+            SuspendLayout();
+            var selected = SelectedEntity;
+            //var ds = entities?.Select(e => new EntityWrapper(e, displayFormat, organizationService)).ToArray();
+            //base.DataSource = ds;
+            //base.Refresh();
+            //if (selected != null && ds.FirstOrDefault(e => e.Entity.Id.Equals(selected.Id)) is EntityWrapper newselected)
+            //{
+            //    SelectedItem = newselected;
+            //}
+            ResumeLayout();
+        }
+
+        public void RetrieveMultiple(QueryBase query, ProgressUpdate progressCallback, RetrieveComplete completeCallback)
+        {
+            if (this.OrganizationService == null)
+            {
+                throw new InvalidOperationException("The Service reference must be set before calling RetrieveMultiple.");
+            }
+
+            try
+            {
+                var worker = new BackgroundWorker();
+                worker.DoWork += (w, e) =>
+                {
+                    var queryExp = e.Argument as QueryBase;
+
+                    BeginInvoke(progressCallback, "Begin Retrieve Multiple");
+
+                    var fetchReq = new RetrieveMultipleRequest
+                    {
+                        Query = queryExp
+                    };
+
+                    var records = OrganizationService.RetrieveMultiple(query);
+
+                    BeginInvoke(progressCallback, "End Retrieve Multiple");
+
+                    e.Result = records;
+                };
+
+                worker.RunWorkerCompleted += (s, e) =>
+                {
+                    var records = e.Result as EntityCollection;
+
+                    BeginInvoke(progressCallback, $"Retrieve Multiple - records returned: {records.Entities.Count}");
+
+                    DataSource = records;
+
+                    // make the final callback
+                    BeginInvoke(completeCallback, entities?.Count(), SelectedEntity);
+                };
+
+                // kick off the worker thread!
+                worker.RunWorkerAsync(query);
+            }
+            catch (System.ServiceModel.FaultException ex)
+            {
+            }
+        }
+
+        public void RetrieveMultiple(string fetchXml, ProgressUpdate progressCallback, RetrieveComplete completeCallback)
+        {
+            RetrieveMultiple(new FetchExpression(fetchXml), progressCallback, completeCallback);
+        }
+
+        #endregion Public Methods
+    }
+}
