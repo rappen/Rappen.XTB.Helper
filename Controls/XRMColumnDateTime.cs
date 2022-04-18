94e3bbe0cc5d64c990425921c064f5b686b4ce95
Jonas Rapp <jonas@jonasr.app>
2022-04-18 15:44:52 +0200

    Added New control: `XRMColumnDataTime`.
    Closed #11

---
+++ Controls/XRMColumnDateTime.cs
@@ -0,0 +1,88 @@
+﻿using Rappen.XTB.Helpers.Interfaces;
+using System;
+using System.ComponentModel;
+using System.Windows.Forms;
+
+namespace Rappen.XTB.Helpers.Controls
+{
+    public partial class XRMColumnDateTime : DateTimePicker, IXRMRecordControl
+    {
+        private string column = string.Empty;
+        private XRMRecordHost recordhost;
+        private bool populating;
+
+        public XRMColumnDateTime()
+        {
+            InitializeComponent();
+            ValueChanged += XRMColumnDateTime_ValueChanged;
+        }
+
+        private void XRMColumnDateTime_ValueChanged(object sender, EventArgs e)
+        {
+            if (DesignMode || populating)
+            {
+                return;
+            }
+            if (recordhost == null)
+            {
+                throw new XRMControlException(this, "RecordHost not set.");
+            }
+            if (string.IsNullOrWhiteSpace(column))
+            {
+                MessageBox.Show("Cannot set value, Column property missing.", this.ToString(), MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            recordhost[column] = Value;
+        }
+
+        [Category("Rappen XRM")]
+        public XRMRecordHost RecordHost
+        {
+            get => recordhost;
+            set
+            {
+                if (recordhost == value)
+                {
+                    return;
+                }
+                recordhost?.RemoveControl(this);
+                recordhost = value;
+                recordhost?.AddControl(this);
+            }
+        }
+
+        [Category("Rappen XRM")]
+        [Description("Column to bind the data time picker to.")]
+        public string Column
+        {
+            get { return column; }
+            set
+            {
+                if (value == column)
+                {
+                    return;
+                }
+                column = value;
+                RecordUpdated();
+            }
+        }
+
+        public void RecordUpdated()
+        {
+            if (DesignMode || recordhost?.Suspended == true)
+            {
+                return;
+            }
+            populating = true;
+            if (recordhost != null && !string.IsNullOrWhiteSpace(column) && recordhost[column] is DateTime value)
+            {
+                Value = value;
+            }
+            else
+            {
+                Value = DateTime.Now;
+            }
+            populating = false;
+        }
+    }
+}
