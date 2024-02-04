d98cdf737ef1870280f6db67513f9885f8d140a3
Jonas Rapp <jonas@jonasr.app>
2024-02-04 10:24:41 +0100

    Added Extensions for Query to help us

---
+++ Rappen.XRM.Helpers/Extensions/QueryExtensions.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Xrm.Sdk.Query;
+using System;
+using System.Xml;
+
+namespace Rappen.XRM.Helpers.Extensions
+{
+    public static class QueryExtensions
+    {
+        public static void NavigatePage(this QueryBase query, string cookie, int diffpage = 1)
+        {
+            if (string.IsNullOrWhiteSpace(cookie) && diffpage != 1)
+            {
+                //throw new Exception("Cannot navigate without a paging cookie.");
+            }
+            if (query is QueryExpression qex)
+            {
+                qex.PageInfo.PageNumber = qex.PageInfo.PageNumber + diffpage;
+                qex.PageInfo.PagingCookie = cookie;
+            }
+            else if (query is FetchExpression fex)
+            {
+                var pagedoc = fex.Query.ToXml();
+                if (pagedoc.SelectSingleNode("fetch") is XmlElement fetchnode)
+                {
+                    if (!int.TryParse(fetchnode.GetAttribute("page"), out int pageno))
+                    {
+                        pageno = 1;
+                    }
+                    pageno = pageno + diffpage;
+                    fetchnode.SetAttribute("page", pageno.ToString());
+                    fetchnode.SetAttribute("pagingcookie", cookie);
+                    fex.Query = pagedoc.OuterXml;
+                }
+            }
+            else
+            {
+                throw new Exception($"Unable to retrieve more pages, unexpected query: {query.GetType()}");
+            }
+        }
+    }
+}
---
+++ Rappen.XTB.Helpers/Extensions/QueryExtensions.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Query;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace Rappen.XTB.Helpers.Extensions
+{
+    public static class QueryExtensions
+    {
+        public static bool WarnIfNoIdReturned(this IEnumerable<Entity> result)
+        {
+            if (result.Any(e => e.Id.Equals(Guid.Empty)))
+            {
+                MessageBox.Show("There are records without an ID, and those records cannot be updated.\r\n\r\nThis is probably because of two things:\r\nRetrieved with a distinct flag, and the ID attribute is not included in the query.\r\n\r\nPlease fix either issue.", "Missing Id", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                return true;
+            }
+            return false;
+        }
+
+        public static bool WarnIf50kReturned(this IEnumerable<Entity> result, string fetch) => WarnIf50kReturned(result, new FetchExpression(fetch));
+
+        public static bool WarnIf50kReturned(this IEnumerable<Entity> result, QueryBase query)
+        {
+            if (result.Count() == 50000)
+            {
+                var fxml = XRM.Helpers.FetchXML.Fetch.FromQuery(query);
+                var numberorlinkedorders = fxml.Entity?.LinkEntities?.Sum(le => le.Orders?.Count() ?? 0) ?? 0;
+                if (numberorlinkedorders > 0)
+                {
+                    MessageBox.Show($"The query has {numberorlinkedorders} orders in link-entities.\nThis means that max 50000 records will be returned from Dataverse.\n\nClick Help button to read more info about 'Legacy paging' limits at Microsoft Learn.", "Retrieving all pages", MessageBoxButtons.OK, MessageBoxIcon.Warning, MessageBoxDefaultButton.Button1, 0,
+                    "https://learn.microsoft.com/power-apps/developer/data-platform/org-service/paging-behaviors-and-ordering?WT.mc_id=DX-MVP-5002475");
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
