ebf19a3fba961fb8d4c23238b2336d3750dfeb60
Jonas Rapp <jonas@jonasr.app>
2024-02-27 16:54:17 +0100

    Moved everything inte ContextEntity.cs - easier to just rip it for you!

--- Rappen.XRM.Helpers/Plugin/ContextEntity.cs
+++ Rappen.XRM.Helpers/Plugin/ContextEntity.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.Xrm.Sdk;
 using System;
+using System.Collections;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace Rappen.XRM.Helpers.Plugin
@@ -12,6 +14,13 @@ namespace Rappen.XRM.Helpers.Plugin
 
         public int Index;
 
+        /// <summary>
+        /// Contructor of ContextEntity to access Target, PreImage, PostImage and Complete
+        /// </summary>
+        /// <param name="context">IPluginExecutionContext5 is all you need to use this class</param>
+        /// <param name="preImageName">If you have a specific pre image name, enter it, otherwise to first one will be used</param>
+        /// <param name="postImageName">If you have a specific post image name, enter it, otherwise to first one will be used</param>
+        /// <param name="index">Index is only used for collection of targets, pre/post images</param>
         public ContextEntity(IPluginExecutionContext5 context, string preImageName = null, string postImageName = null, int index = -1)
         {
             this.context = context;
@@ -20,6 +29,11 @@ namespace Rappen.XRM.Helpers.Plugin
             Index = index;
         }
 
+        /// <summary>
+        /// Get the Entity by type
+        /// </summary>
+        /// <param name="type"></param>
+        /// <returns></returns>
         public Entity this[ContextEntityType type]
         {
             get
@@ -96,8 +110,39 @@ namespace Rappen.XRM.Helpers.Plugin
         }
     }
 
+    public class ContextEntityCollection : IEnumerable<ContextEntity>
+    {
+        private List<ContextEntity> _entities = new List<ContextEntity>();
+
+        /// <summary>
+        /// Contructor of ContextEntityCollection to access a list of Targets, PreImages, PostImages and Complete
+        /// </summary>
+        /// <param name="context">IPluginExecutionContext5 is all you need to use this class</param>
+        /// <param name="preImageName">If you have a specific pre image name, enter it, otherwise to first one will be used</param>
+        /// <param name="postImageName">If you have a specific post image name, enter it, otherwise to first one will be used</param>
+        public ContextEntityCollection(IPluginExecutionContext5 context, string preImageName = null, string postImageName = null)
+        {
+            if (context?.InputParameters?.Contains(ParameterName.Targets) == true &&
+                context.InputParameters[ParameterName.Targets] is EntityCollection entityCollection)
+            {
+                var i = 0;
+                entityCollection.Entities.ToList().ForEach(_ => _entities.Add(new ContextEntity(context, preImageName, postImageName, i++)));
+            }
+        }
+
+        public IEnumerator<ContextEntity> GetEnumerator() => _entities.GetEnumerator();
+
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+    }
+
     public static class ContextEntityExtensions
     {
+        /// <summary>
+        /// Simple, but smart, Merge extension to Entity
+        /// </summary>
+        /// <param name="entity1"></param>
+        /// <param name="entity2"></param>
+        /// <returns></returns>
         public static Entity Merge(this Entity entity1, Entity entity2)
         {
             var merge = entity1.Clone(false) ?? entity2.Clone(false);
@@ -108,6 +153,12 @@ namespace Rappen.XRM.Helpers.Plugin
             return merge;
         }
 
+        /// <summary>
+        /// Simple, but smart, Clone
+        /// </summary>
+        /// <param name="entity">Entity to clone</param>
+        /// <param name="onlyId">True if only Id shall be used, otherwise all attributes will be include</param>
+        /// <returns></returns>
         public static Entity Clone(this Entity entity, bool onlyId)
         {
             if (entity == null)
