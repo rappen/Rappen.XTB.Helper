1634e24eb12e727e3220a2599928e7ab410d7177
Jonas Rapp <jonas@jonasr.app>
2024-09-03 16:17:31 +0200

    Added extensions ToXmlNode

--- Rappen.XRM.Helpers/Extensions/XmlExtensions.cs
+++ Rappen.XRM.Helpers/Extensions/XmlExtensions.cs
@@ -21,6 +21,23 @@ namespace Rappen.XRM.Helpers.Extensions
             return xmldoc;
         }
 
+        public static XmlNode ToXmlNode(this string XmlString, bool IgnoreXmlErrors = true)
+        {
+            var xmldoc = new XmlDocument();
+            try
+            {
+                xmldoc.LoadXml(XmlString);
+            }
+            catch (XmlException e)
+            {
+                if (!IgnoreXmlErrors)
+                {
+                    throw e;
+                }
+            }
+            return xmldoc.DocumentElement;
+        }
+
         public static string AttributeValue(this XmlNode node, string key)
         {
             if (node != null && node.Attributes != null && node.Attributes[key] is XmlAttribute attr)
