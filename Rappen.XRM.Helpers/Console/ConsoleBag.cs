f01d54db97ee5bc18c10704fa60ffb509ccf4fc7
Jonas Rapp <jonas@jonasr.app>
2024-12-20 13:49:16 +0100

    Added Console Bag and Logger

---
+++ Rappen.XRM.Helpers/Console/ConsoleBag.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Xrm.Sdk;
+using Rappen.XRM.Helpers.Interfaces;
+
+namespace Rappen.XRM.Helpers.Console
+{
+    public class ConsoleBag : IBag
+    {
+        private ConsoleLogger logger;
+
+        public ILogger Logger => logger;
+
+        public IOrganizationService Service { get; }
+
+        public ConsoleBag(IOrganizationService service)
+        {
+            Service = service;
+            logger = new ConsoleLogger();
+        }
+        public void Trace(string format, params object[] args) => logger.Log(string.Format(format, args));
+    }
+}
---
+++ Rappen.XRM.Helpers/Console/ConsoleLogger.cs
@@ -0,0 +1,28 @@
+﻿using Rappen.XRM.Helpers.Interfaces;
+using System;
+
+namespace Rappen.XRM.Helpers.Console
+{
+    public class ConsoleLogger : ILogger
+    {
+        public void EndSection()
+        {
+            System.Console.WriteLine("/");
+        }
+
+        public void Log(string message)
+        {
+            System.Console.WriteLine(message);
+        }
+
+        public void Log(Exception ex)
+        {
+            System.Console.WriteLine(ex.Message);
+        }
+
+        public void StartSection(string name = null)
+        {
+            System.Console.WriteLine("\\" + (name ?? ""));
+        }
+    }
+}
