1540742072ae493172f73fc867d34521d27b06f1
Jonas Rapp <jonas@jonasr.app>
2025-10-21 10:44:45 +0200

    Added option to send by <enter>

--- Rappen.AI.WinForm/AISettings.cs
+++ Rappen.AI.WinForm/AISettings.cs
@@ -14,6 +14,7 @@ namespace Rappen.AI.WinForm
         public int Calls { get; set; }
         public bool LogConversation { get; set; }
         public bool PreferDisplayName { get; set; }
+        public bool SendWithEnter { get; set; }
 
         public override string ToString() => $"{Provider} - {Model} - {Endpoint} - {ApiKey} - {MyName} - {PreferDisplayName}";
     }
